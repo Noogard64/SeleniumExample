30f1109c77761988fec5ab50b48dca80c53050cf
Sean Phillips <sean-phillips@live.com>
2017-12-06 20:51:39 -0500

    Add project files.

---
+++ SeleniumExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumExample")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8534155f-388f-4e71-8a27-ad442df82972")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumExample/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿namespace Partsunlimited.UITests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.IE;
+    using OpenQA.Selenium.Remote;
+    using OpenQA.Selenium.PhantomJS;
+    using System;
+
+    [TestClass]
+    public class ChucksClass1
+    {
+        private string baseURL = "http://www.google.com";
+        private RemoteWebDriver driver;
+        private string browser;
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        [Priority(1)]
+        //[Owner("FireFox")]
+
+        public void TireSearch_Any()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            driver.Navigate().GoToUrl(this.baseURL);
+            //driver.FindElementById("search - box").Clear();
+            //driver.FindElementById("search - box").SendKeys("tire");
+            //do other Selenium things here!
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+        }
+    }
+}
